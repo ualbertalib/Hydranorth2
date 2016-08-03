278241c6b4c061687ad2fbc0751433fc2406d1e1
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2016-08-03 11:24:43 -0600

    setup for testing collection/solr join behaviour resolves ualbertalib/hydranorth#1154  * start solr_wrapper in cloud mode  * add verbose to fcrepo_wrapper  * seed 340 collections with 100 works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+files = []
+100.times do |j|
+  files << GenericWork.new do |work|
+    work.title = ["Jack's Research"]
+    work.keyword = ['jacks_keyword']
+    work.apply_depositor_metadata('tricia.jenkins@ualberta.ca')
+    work.read_groups = ['public']
+    work.save!
+  end
+end
+
+430.times do |i|
+  Collection.new(title: ["Collection #{i}"]).tap do |c|
+    c.apply_depositor_metadata "tricia.jenkins@ualberta.ca"
+    c.members << files.sample(rand(10...100))
+    c.save!
+  end
+end
