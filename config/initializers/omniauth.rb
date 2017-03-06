66279ec7d3b007f289829ad34f1b477751906cd6
Shane Murnaghan <murnagha@gmail.com>
2017-03-06 16:24:20 -0700

    Fix urls and add SP certificate for encrypted assertions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
-           assertion_consumer_service_url: 'https://era.library.ualberta.com/auth/saml/callback',
+           assertion_consumer_service_url: 'https://era.library.ualberta.ca/auth/saml/callback',
            idp_cert: 'MIIEUzCCAzugAwIBAgIJAJ0Fn4JwMfuOMA0GCSqGSIb3DQEBCwUAMIG/MQswCQYDVQQGEwJDQTEQMA4GA1UECAwHQW'\
            'xiZXJ0YTERMA8GA1UEBwwIRWRtb250b24xHjAcBgNVBAoMFVVuaXZlcnNpdHkgb2YgQWxiZXJ0YTEqMCgGA1UECwwhSW5mb3JtYXRpb24'\
            'gU2VydmljZXMgJiBUZWNobm9sb2d5MRowGAYDVQQDDBFsb2dpbi51YWxiZXJ0YS5jYTEjMCEGCSqGSIb3DQEJARYUdGFudG9uaW9AdWFs'\
@@ -16,9 +16,30 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            'SdKWW0yk6TEV887QXKWD8NgqFztetIZk2c0r7KxcELvMPamnrd6HgmGlMl5ajNKVf/9VRkXBVrigDWYFiFDaskpfU4dhKIn41pvx1ZF/Z'\
            'Bv9UHNNDXoHLRQKjQeoARtqUeS0ISh8/zcONfjyOqhK3D3IHjwQA11Fhxx/bwyYxnfPWpSG7oPhSvO42KAuruyR544NwaaBBx0KkfaoZr'\
            'WMXHa4Ddq6h762kJ4ducpXZTzPQ==',
+           certificate: '-----BEGIN CERTIFICATE-----
+MIIDQDCCAiigAwIBAgIJAOCm/waPAgtVMA0GCSqGSIb3DQEBBQUAMCQxIjAgBgNV
+BAMTGXBsYW5vLmxpYnJhcnkudWFsYmVydGEuY2EwHhcNMTUxMDAxMjEwMjQ5WhcN
+MjUwOTI4MjEwMjQ5WjAkMSIwIAYDVQQDExlwbGFuby5saWJyYXJ5LnVhbGJlcnRh
+LmNhMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsxRH61rSVigXTECa
+PD0Dym6+UvEiL1WQc8ST0jYzmYlU90PCN6zbxmLTM8LEDsAKBeC3niMlXzRtFm08
+u1yessogDwRGKdUAmMuGGZOAj4kLSqUUp/LWObOBSPmVXF//P/XsNTbIIPWee5jG
+0K98gegzHdBf9BTv/ytGdjzNVeUcqWtazGqWcy0smAc6NZjdwx3FMoQJmZtzR1P7
+lZOvGrCtczpHVwCZapD8lWVYjGZM1ww4PzUiyV2kunW/bn0wV25wzOo86JZKqfPh
+bMLRvupHuS0bqlLwPAbB2Vqp4ANgitKHaBX0TadjOVCbDei0d/iLMzh3aRQ8XngB
+HdDyrQIDAQABo3UwczBSBgNVHREESzBJghlwbGFuby5saWJyYXJ5LnVhbGJlcnRh
+LmNhhixodHRwczovL3BsYW5vLmxpYnJhcnkudWFsYmVydGEuY2Evc2hpYmJvbGV0
+aDAdBgNVHQ4EFgQUY6PzGBavmQEs7H+x5XdinkoxQnUwDQYJKoZIhvcNAQEFBQAD
+ggEBAGn/0WiiOaONMIpIDj9KfCzZAjW4AQmCaaSvVqfRnXc8BX+oV3eKUcToyT8s
+slG867fCdiIODQH3JsSrPkwztnPee5kwRcsd7/tpjs8lrzcTwTPAbdU5ZyiF/tCw
+H1tpkYu0VnY/DKzny5iTSs6ThBesaDUESReDNMIZNnB6QnSByI6fstqs0rhxdMzX
+04LeE4CwVHhM7EdMBa2ZS5BGB52o+Z2cmMZQqFv6WM7X+fJyQWaD06hfmtMvyP4o
+vpRsweeCLCQfCxE+5iA2zZCOexzkq5OMWDHsmdeHmwwNkVo1EvhTAb3khH/6yMzV
+i0fDCxtn9InCdldL+Vp+Pnt/Gns=
+-----END CERTIFICATE-----',
+           private_key: '<<<REDACTED>>>',
            idp_slo_target_url: 'https://login.ualberta.ca/saml2/idp/SingleLogoutService.php',
            idp_sso_target_url: 'https://login.ualberta.ca/saml2/idp/SSOService.php',
-           issuer: 'https://era.library.ualberta.com',
+           issuer: 'https://era.library.ualberta.ca',
            name_identifier_format: 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
            request_attributes: [
              { name: 'mail',
