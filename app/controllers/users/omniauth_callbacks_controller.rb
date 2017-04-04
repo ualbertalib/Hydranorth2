04c5121ed3c903a36c356b7b2398fa2e72f0bcdb
astrilet <astrilet@gmail.com>
2017-04-04 13:20:00 -0600

    small config changes; added env variable to enable temp login in production

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Users::OmniauthCallbacksController < ApplicationController
   # TODO: Remove this once we figure out IST, this is only required for the omniauth developer provider
-  skip_before_action :verify_authenticity_token, only: :complete unless Rails.env.production?
+  skip_before_action :verify_authenticity_token, only: :complete unless (Rails.env.production? && !ENV['ENABLE_TMP_LOGINS'])
 
   def complete
     auth_hash = request.env['omniauth.auth']
